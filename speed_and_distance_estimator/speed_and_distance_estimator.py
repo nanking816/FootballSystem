3322c00ed7be2c904418ef8ac2d994c040562de2
nanking816 <145945624+nanking816@users.noreply.github.com>
2025-04-13 12:50:33 +0200

    Update speed_and_distance_estimator.py

--- speed_and_distance_estimator/speed_and_distance_estimator.py
+++ speed_and_distance_estimator/speed_and_distance_estimator.py
@@ -6,7 +6,7 @@ from utils import measure_distance ,get_foot_position
 class SpeedAndDistance_Estimator():
     def __init__(self):
         self.frame_window=5
-        self.frame_rate=24
+        self.frame_rate=30
     
     def add_speed_and_distance_to_tracks(self,tracks):
         total_distance= {}
